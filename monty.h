504aeff92f0ee7bf55cfe25576fa32788b644481
Tunde <tundeogunmodede1@gmail.com>
2024-01-18 12:53:02 -0800

    fixed

--- monty.h
+++ monty.h
@@ -67,8 +67,8 @@ void f_pall(stack_t **stack, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
 void f_pint(stack_t **stack, unsigned int counter);
 void f_pop(stack_t **stack, unsigned int counter);
-void pchar(stack_t **stack, unsigned int line_number);
-void processLine(char *line, unsigned int line_number, stack_t **stack);
+/**void pchar(stack_t **stack, unsigned int line_number);
+void processLine(char *line, unsigned int line_number, stack_t **stack);*/
 void f_mul(stack_t **stack, unsigned int counter);
 void f_sub(stack_t **stack, unsigned int counter);
 void f_nop(stack_t **stack, unsigned int counter);
