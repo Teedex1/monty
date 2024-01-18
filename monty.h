16cc562027dac75191f7e2eac0deb86648bd011b
Michael Gnaho <gnahomichael@gmail.com>
2024-01-18 21:34:32 +0100

    pchar.c

--- monty.h
+++ monty.h
@@ -67,4 +67,6 @@ void f_pall(stack_t **stack, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
 void f_pint(stack_t **stack, unsigned int counter);
 void f_pop(stack_t **stack, unsigned int counter);
+void pchar(stack_t **stack, unsigned int line_number);
+void processLine(char *line, unsigned int line_number, stack_t **stack);
 #endif
